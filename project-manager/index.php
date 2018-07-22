7970c5f699cc476d5f69cff7b7d37f234ed9f8c3
Thomas Dattilo <tom@dattilo.io>
2018-07-22 19:53:35 -0400

    finds

---
+++ project-manager/index.php
@@ -0,0 +1,2 @@
+<?php header( 'Location: /homePage.html' ) ;  ?>
+
---
+++ src/main/java/ateam/ElementRepository.java
@@ -0,0 +1,9 @@
+package ateam;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ateam.Element;
+
+public interface ElementRepository extends CrudRepository<Element, Integer>{
+
+}
